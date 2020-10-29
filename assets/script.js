3da00cc226dcddcc98d9ff50c17a297c064b4bd2
Jared Ackley <jrod3323@gmail.com>
2020-10-29 14:26:23 -0500

    add JS variables to grab necessary elements

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,25 @@
-var quizStartEl = document.getElementById("quizStart");
+// header element
+var timerEl = document.getElementById("timer");
+
+// Intro Secetion elements and buttons
+var quizStartBtn = document.getElementById("quizStart");
 var introEl = document.getElementById("intro");
+
+// Quiz Questions Section Elements and buttons
 var quizQuestionsEl = document.getElementById("quiz-questions");
+var answer1Btn = document.getElementById("answer1");
+var answer2Btn = document.getElementById("answer2");
+var answer3Btn = document.getElementById("answer3");
+var answer4Btn = document.getElementById("answer4");
+var questionEl = document.getElementById("question");
 
+// Final Score Sections Elements and Buttons
+var yourScoreEl = document.getElementById("yourScore");
+var submitScoreBTN = document.getElementById("submitScore");
+var finalScoreEl = document.getElementById("finalScore");
+var goBackBtn = document.getElementById("goBack");
 
-quizStartEl.addEventListener("click",quizStart);
+quizStartBtn.addEventListener("click",quizStart);
 
 function quizStart(event){
     quizQuestionsEl.style.display = "flex"
